558db37ad246e4abb10e4496b6b9557aeb529543
Junior Rocha <jr@opara.me>
2024-02-19 22:06:40 -0300

    inserindo novos plugins

--- wp-setup.sh
+++ wp-setup.sh
@@ -13,6 +13,7 @@ wp plugin install simple-history --allow-root --activate
 wp plugin install admin-site-enhancements --allow-root --activate
 wp plugin install rollback-update-failure --allow-root --activate
 wp plugin install performance-lab --allow-root --activate
+wp plugin install webp-uploads --allow-root --activate
 wp plugin install wordfence --allow-root --activate
 wp plugin install all-in-one-wp-migration --allow-root --activate
 wp plugin install https://opara.nyc3.cdn.digitaloceanspaces.com/public/plugins/all-in-one-wp-migration-unlimited-extension-v2.54.zip --allow-root --activate
