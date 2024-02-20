87633f816c4f3129dfc43d7a32e96f973e60af2f
Junior Rocha <jr@opara.me>
2024-02-19 22:02:47 -0300

    atualizando script

--- wp-setup.sh
+++ wp-setup.sh
@@ -11,6 +11,8 @@ wp option update timezone_string "America/Sao_Paulo" --allow-root
 ### Install plugins from official repository
 wp plugin install simple-history --allow-root --activate
 wp plugin install admin-site-enhancements --allow-root --activate
+wp plugin install rollback-update-failure --allow-root --activate
+wp plugin install performance-lab --allow-root --activate
 wp plugin install wordfence --allow-root --activate
 wp plugin install all-in-one-wp-migration --allow-root --activate
 wp plugin install https://opara.nyc3.cdn.digitaloceanspaces.com/public/plugins/all-in-one-wp-migration-unlimited-extension-v2.54.zip --allow-root --activate
